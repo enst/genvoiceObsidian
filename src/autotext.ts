6a9e238a95a45e7cd145d0aa1d369ca8cc7796f5
JeffreyQin <122770444+JeffreyQin@users.noreply.github.com>
2023-06-02 10:32:53 -0400

    fixed auto text insertion
    - will insert edit date and name automatically if
    1. No templates detected
    2. cursor not currently in the template header area

--- src/autotext.ts
+++ src/autotext.ts
@@ -4,14 +4,29 @@ import { TextPluginSettings } from './settings'
 // detect if cursor is currently in the dataview area
 
 export function disabledArea(app: App, editor: Editor, settings: TextPluginSettings): boolean {
-    let lineTrack = 0;
+    let numTrack = 0;
     for (let index = 0; index < editor.getCursor().line - 1; index++) {
         if (editor.getLine(index).startsWith(settings.dataviewHeaderLine)) {
-            lineTrack ++;
+            numTrack ++;
         }
     }
-    if (lineTrack <= 1) {
+    if (numTrack == 1) {
         return true;
+    }
+    if (numTrack == 0 && (editor.getLine(editor.getCursor().line).startsWith(settings.dataviewHeaderLine)
+        || editor.getLine(editor.getCursor().line - 1).startsWith(settings.dataviewHeaderLine))) {
+            return true;
+    }
+    return false;
+}
+
+// check if the cursor is currently in the newest date 
+
+export function notNewestDate(app: App, editor: Editor, settings: TextPluginSettings): boolean {
+    for (let index = 0; index < editor.getCursor().line; index++) {
+        if (editor.getLine(index).startsWith(settings.separationLineStr)) {
+            return true;
+        }
     }
     return false;
 }
@@ -19,19 +34,10 @@ export function disabledArea(app: App, editor: Editor, settings: TextPluginSetti
 //generate auto text (date + username)
 
 export function generateAutoText(app: App, editor: Editor, settings: TextPluginSettings) {
-    if (!disabledArea(app, editor, settings)) {
-        let lineTrack = true;
-        for (let index = 0; index < editor.getCursor().line; index ++) {
-            if (editor.getLine(index).startsWith(settings.separationLineStr)) {
-                lineTrack = false;
-                break;
-            }
-        }
-        if (lineTrack) {
-            editor.replaceRange(
-                '\n\n' + settings.separationLineStr + '\n' + moment().format(settings.dateFormat) + ' ' + settings.username,
-                { line: editor.getCursor().line - 1, ch: 0 }
-            )
-        }
+    if (!disabledArea(app, editor, settings) && !notNewestDate(app, editor, settings)) {
+        editor.replaceRange(
+            '\n\n' + settings.separationLineStr + '\n' + moment().format(settings.dateFormat) + ' ' + settings.username,
+            { line: editor.getCursor().line - 1, ch: 0 }
+        )
     }
 }
